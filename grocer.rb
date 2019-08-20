cca6b3deef53dcd5328e9f4326aeef608207eeef
Arlene George <aemg@live.com>
2019-08-20 04:38:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,6 +43,17 @@ end
 
 
 def checkout(cart, coupons)
-  new_cart = consolidate_cart(cart)
+  one = consolidate_cart(cart)
+  two = apply_coupons(one, coupons)
+  three = apply_clearance(two)
+  
+  total = 0
+  
+  three.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+  
+  total > 100 ? total * 0.9 : total
+  
 end 
 
